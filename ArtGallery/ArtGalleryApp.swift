cd0ad00de1c2682579633d5d4bd3f8be24c761ed
Deannv <afilfriend@gmail.com>
2025-11-28 14:39:36 +0800

    Initial Commit

---
+++ ArtGallery/ArtGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtGalleryApp.swift
+//  ArtGallery
+//
+//  Created by Kemas Deanova on 28/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ArtGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArtGalleryTests/ArtGalleryTests.swift
@@ -0,0 +1,17 @@
+//
+//  ArtGalleryTests.swift
+//  ArtGalleryTests
+//
+//  Created by Kemas Deanova on 28/11/25.
+//
+
+import Testing
+@testable import ArtGallery
+
+struct ArtGalleryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
